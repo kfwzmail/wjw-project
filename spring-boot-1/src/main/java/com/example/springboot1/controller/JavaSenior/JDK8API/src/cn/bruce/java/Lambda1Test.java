3e04678d21a8dce27a61d5bd2e670daf7a513bc1
wjw <kfwz1208@gmail.com>
2024-03-23 20:34:58 +0800

    java8

--- spring-boot-1/src/main/java/com/example/springboot1/controller/JavaSenior/JDK8API/src/cn/bruce/java/Lambda1Test.java
+++ spring-boot-1/src/main/java/com/example/springboot1/controller/JavaSenior/JDK8API/src/cn/bruce/java/Lambda1Test.java
@@ -6,31 +6,38 @@ import java.util.Comparator;
 
 /***
  * @Author wjw
- * @Description Lambda表达式的使用举例
+ * @Description 使用Lambda表达式前后对比
  * @Date 2024/3/14 07:13
  * @Param
  * @return null
  */
-public class LambdaTest1 {
+public class Lambda1Test {
+    /***
+     * @Author wjw
+     * @Description 调用 Runnable 接口
+     * @Date 2024/3/23 19:28
+     * @Param
+     */
     @Test
     public void test1() {
-        //未使用Lamdba表达式的写法
+        //未使用Lambda表达式的写法
         Runnable r1 = new Runnable() {
             @Override
             public void run() {
-                System.out.println("hello Lamdba!");
+                System.out.println("hello Lambda!");
             }
         };
         r1.run();
         System.out.println("========================");
-        //Lamdba表达式写法
-        Runnable r2 = () -> System.out.println("hi Lamdba!");
+
+        //Lambda表达式写法
+        Runnable r2 = () -> System.out.println("hi Lambda!");
         r2.run();
     }
 
     @Test
     public void test2() {
-        //未使用Lamdba表达式的写法
+        //未使用Lambda表达式的写法
         Comparator<Integer> com1 = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -38,19 +45,21 @@ public class LambdaTest1 {
             }
         };
         int compare1 = com1.compare(12, 32);
-        //-1
+        //结果：-1
         System.out.println(compare1);
         System.out.println("===================");
+
         //Lambda表达式的写法
         Comparator<Integer> com2 = (o1, o2) -> Integer.compare(o1, o2);
         int compare2 = com2.compare(54, 21);
-        //1
+        //结果：1
         System.out.println(compare2);
         System.out.println("===================");
+
         //方法引用
         Comparator<Integer> cpm3 = Integer::compareTo;
         int compare3 = cpm3.compare(12, 12);
-        //0
+        //结果：0
         System.out.println(compare3);
     }
 }
